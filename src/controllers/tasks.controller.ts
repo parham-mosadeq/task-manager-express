d56d071b9a3588012f0c585ffb599518b54bc3f0
= <pmosadeqzadeh@gmail.com>
2025-03-27 20:45:12 +0330

    chore: get single and all

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,12 +1,29 @@
 import { Request, Response } from "express";
 import { TaskModel } from "@models/task.schema";
 
-const getAllTasks = (req: Request, res: Response) => {
-  res.send({ status: "success", data: [] });
+const getAllTasks = async (req: any, res: any) => {
+  try {
+    const allTasks = await TaskModel.find();
+    return res.status(200).json({ status: "success", data: allTasks });
+  } catch (error: any) {
+    if (error?.errors)
+      return res
+        .status(400)
+        .json({ status: "error", message: error.errors.name.message });
+  }
 };
 
-const getSingleTask = (req: Request, res: Response) => {
-  res.send({ status: "success", data: [] });
+const getSingleTask = async (req: any, res: any) => {
+  try {
+    const { id: taskID } = req.params;
+    const currentTask = await TaskModel.findOne({ _id: taskID });
+    console.log(currentTask, "------currentTask");
+    return res.status(200).json({ status: "success", data: [currentTask] });
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "No value found by this id" });
+  }
 };
 
 const createTask = async (req: any, res: any) => {
